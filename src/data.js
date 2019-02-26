5bfe536c0f85b0cafcd90f3269960ed25300216a
Jesús 22 <jjuantteran@gmail.com>
2019-02-26 11:16:41 -0600

    Se agrego la funcion filterImageOponnent para filtrar el nombre e imagen del Pokemon contrario

--- src/data.js
+++ src/data.js
@@ -69,7 +69,14 @@ que de ahi, acceda solamente a la imagen*/
 // let imagePokemon = '';
 const filterImage = (listPokemon, selectPokemon) => {  
   const image = listPokemon.filter(pokemon => (pokemon.name == selectPokemon));
-  console.log(image[0].img);
+  //console.log(image[0].img);
   return image[0].img;
 }
 // console.log(filterImage(namePokemon,'img'));
+
+
+const filterImageOpponent = (nameOpponent, selectOpponent) => {  
+  const image = nameOpponent.filter(pokemon => (pokemon.name == selectOpponent));
+  //console.log(image[0].img);
+  return image[0].img;
+}
