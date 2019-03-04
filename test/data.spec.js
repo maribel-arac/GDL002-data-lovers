95e419144ceee3d255fbf2d9232bee6cc7d9a06a
maribel-arac <maribel.arca@gmail.com>
2019-03-03 23:18:26 -0600

    los tests de sort y filtrar, terminados

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,12 +1,23 @@
 require('../src/data.js');
+const pokemon = require ('../src/data/pokemon/pokemon.json');
+const filterData = require ('../src/data/pokemon/filterData.json');
+const candiesData = require ('../src/data/pokemon/candiesFilter.json');
 
-
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+describe("filterData", ()=>{
+it("Should be a function", ()=>{
+    expect(typeof window.allPokemons.filterData).toBe("function");
   });
+  it("Should return one pokemon Bulbasaur", ()=>{
+    expect(window.allPokemons.filterData(pokemon.pokemon, 'Bulbasaur')).toEqual(filterData);
+  });
+});
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+
+describe("computeStats", () => {
+  it("Should be a function", () => {
+    expect(typeof window.allPokemons.computeStats).toBe("function");
+  });
+  it("Should return candy_count substraction", () => {
+    expect(window.allPokemons.computeStats(pokemon.pokemon)).toEqual(candiesData);
   });
 });
