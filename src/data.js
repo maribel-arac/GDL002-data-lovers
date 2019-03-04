55b00ae33d000ad4eb01b7f033b06b261c61ce04
maribel-arac <maribel.arca@gmail.com>
2019-03-03 22:36:20 -0600

    se agrego la función para ordernar alfabeticamente los pokemons

--- src/data.js
+++ src/data.js
@@ -1,23 +1,32 @@
+window.allPokemons = {
+  /*Función para filtrar los pokemones por imagen. Se crea una variable para  filtrar los pokemones donde 
+  se compara con el nombre del pokemon seleccionado por el usuario y despues para entrar al objeto, se comienza en
+  [0] y de ahi, acceder solo a la imagen que pertenece al Pokemon seleccionado*/
+  filterData: (listPokemon, selectPokemon) => {  
+    //ESta función debe de recibir una data (arreglos de objetos) y un String (el nombre del pokemon)
+    const data = listPokemon.filter(pokemon => (pokemon.name == selectPokemon));
+    // Y retorna un objeto pokemon
+    return data[0];
 
-/*Función para filtrar los pokemones por imagen. Se crea una variable para  filtrar los pokemones donde 
-se compara con el nombre del pokemon seleccionado por el usuario y despues para entrar al objeto, se comienza en
- [0] y de ahi, acceder solo a la imagen que pertenece al Pokemon seleccionado*/
- 
-// console.log(photoPokemons);
-// let imagePokemon = '';
-const filterData = (listPokemon, selectPokemon) => {  
-  //ESta función debe de recibir una data (arreglos de objetos) y un String (el nombre del pokemon)
-  const data = listPokemon.filter(pokemon => (pokemon.name == selectPokemon));
-  //console.log(image[0].name);
+  },
 
-  // Y retorna un objeto pokemon
-  return data[0];
-}
+  //Función es para saber cuantos candies le faltan al usuario para evolucionar su pokemon
+  computeStats: (objectPokemon, selectPokemon, candies) => {
+    //console.log(objectPokemon, selectPokemon, candies);
+    return objectPokemon.find(pokemon => 
+      pokemon.name === selectPokemon[selectPokemon.selectedIndex].innerText).candy_count - candies;
+      
+    },
+    sortData: (allPokemons) => {
 
-//esta función es para saber cuantos candies le faltan al usuario para evolucionar su pokemon
- const computerData = (objectPokemon, selectPokemon, candies) => {
-  // console.log(objectPokemon);
-   console.log(objectPokemon.find(pokemon => pokemon.name === selectPokemon[selectPokemon.selectedIndex].innerText).candy_count); 
-   return objectPokemon.find(pokemon => pokemon.name === selectPokemon[selectPokemon.selectedIndex].innerText).candy_count - candies;
- }
-  
+      let orderAllName = allPokemons.sort((a, b) => {
+        if (a.name > b.name) {
+          return 1;
+        }
+        if (a.name < b.name) {
+          return -1;
+        }
+      });
+      return orderAllName;
+    },
+  };
